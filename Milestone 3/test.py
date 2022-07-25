1aebce5d3b48f848d91ce61b9da73398d1cb6a52
jing-rui324 <106535027+jing-rui324@users.noreply.github.com>
2022-07-25 21:57:57 +0800

    Update test.py

--- Milestone 3/test.py
+++ Milestone 3/test.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
 
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    IntLvl = int(request.form['IntLvl'])
+    ComLvl = int(request.form['ComLvl'])
+    Age = int(request.form['Age'])
+    prediction = model.predict([[IntLvl, ComLvl, Age]])
+    output = round(prediction[0], 0)
+    return render_template('index.html', prediction_text=f'Your Hobbyscore is {output}')
+
+
+if __name__ == "__main__":
+    app.run()
